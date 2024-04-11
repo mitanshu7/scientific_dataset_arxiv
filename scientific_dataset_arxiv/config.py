6ac01a9fad84c4253f44a50636cb8e105a2eafbe
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2024-04-11 09:50:28 +0530

    config routine

--- scientific_dataset_arxiv/config.py
+++ scientific_dataset_arxiv/config.py
@@ -1,8 +1,8 @@
 #####################################################################################################################
 ## Enter the year range for which you want to download the papers and convert them to text files
 ## The year range is inclusive, valid for yymm >=0704. Arxiv metadata starts from 2007-04
-start_year = 2019
-end_year = 2023
+start_year = 2014
+end_year = 2018
 #####################################################################################################################
 ## Here you can set the maximum number of PDFs to download and convert per month
 ## The default value is 10000. In the merged dataset, you will get about the 50% of the maximum value.
